91607b350cf4278b15708ca697831d1e2685e0dd
Chris Williams <cchriswilliams721@gmail.com>
2024-12-11 20:37:56 -0700

    auth finished

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,9 +2,28 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useUserAuth } from "@/app/_utils/auth-context";
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { auth } from "@/app/_utils/firebase";
 
 export default function Nav() {
   const router = useRouter();
+  const {user, logOut, signIn} = useUserAuth();
+
+  const handleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth); // Pass auth if you're using firebase's signOut
+      router.push("/");
+    } catch (error) {
+      console.error("Logout Error: ", error);
+    }
+  };
+
   return (
     <div className="h-24 flex items-center justify-between px-4 shadow-md">
       {/* LEFT */}
@@ -34,14 +53,25 @@ export default function Nav() {
         </Link>
       </div>
       {/* RIGHT */}
-      <div className="w-full md:w-[30%] flex items-center justify-end gap-4 text-sm"> 
+      <div className="w-full md:w-[30%] flex items-center justify-end gap-4 text-sm">
+        {user ? (
+          <>
+            <span className="font-medium">{user?.displayName || "Anonymous"}</span>
+            <button
+              onClick={handleLogout}
+              className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition"
+            >
+              Logout
+            </button>
+          </>
+        ) : (
           <button
-            
+            onClick={handleSignIn}
             className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
           >
             Sign In with Google
           </button>
-        
+        )}
       </div>
     </div>
   );
