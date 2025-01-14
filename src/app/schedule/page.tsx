f411141a76352c127e13cf25526b2bcc15bb4d7c
Chris Williams <cchriswilliams721@gmail.com>
2025-01-14 10:41:41 -0700

    added buttons

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Schedule() {
+    const handleSchedule = () => {
+        
+    }
   return (
-    <div>Schedule</div>
-  )
+    <div>
+      <div className="flex justify-end ">
+        <div className="flex px-4 ">
+          <button className="bg-blue-600 w-32 h-10 rounded-full ">Schedule</button>
+        </div>
+        <div className="flex px-4">
+          <button className="bg-blue-600 rounded-full w-32 h-10">Manage Users</button>
+        </div>
+      </div>
+    </div>
+  );
 }
