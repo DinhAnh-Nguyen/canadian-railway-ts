f411790d1f3cfaf65b95c87ee894c68d4308f52e
Chris Williams <cchriswilliams721@gmail.com>
2025-01-15 14:31:23 -0700

    added fetch all,  add task and modify task

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,6 +1,15 @@
+import { neon } from "@neondatabase/serverless";
+//Add a task - Daniel
 
-//Add a Schedule - Daniel
+//Modify a task - Chris
+export async function PUT( request : Request, { params }: { params: { id: Number | String} }) {
+    const databaseUrl = process.env.DATABASE_URL || ""; // Set a default value if DATABASE_URL is not defined
+    const sql = neon(databaseUrl);
+    //PostgresQL
+    const id = Number(params.id);
+    const resquestData = await request.json();
+    const response = await sql`UPDATE schedules SET description = ${resquestData.description}, status = ${resquestData.status}, due_date = ${resquestData.due_date}, piroty = ${resquestData.piroty} WHERE id = ${id};`;;
+    return new Response(JSON.stringify(response), { status: 200 });
+}
 
-//Modify a Schedule - Chris
-
-//Delete a Schedule - Nathan
+//Delete a task - Nathan
--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,7 +1,25 @@
-//Fetch All Schedules - Mark
+import { neon } from "@neondatabase/serverless";
+
+//Fetch All tasks - Mark
+export async function GET() {
+    const databaseUrl = process.env.DATABASE_URL || ""; // Set a default value if DATABASE_URL is not defined
+    const sql = neon(databaseUrl);
+    //PostgresQL
+    const response = await sql`SELECT * FROM schedules;`;
+    return new Response(JSON.stringify(response), { status: 200 });
+}
 
 // Add task - Daniel
+export async function POST(request: Request) {
+    const resquestData = await request.json();
+    const databaseUrl = process.env.DATABASE_URL || ""; // Set a default value if DATABASE_URL is not defined
+    const sql = neon(databaseUrl);
+    //PostgresQL
+    const response = await sql`INSERT INTO schedules (description, status, due_date, piroty) VALUES (${resquestData.description}, ${resquestData.status}, ${resquestData.due_date}, ${resquestData.piroty});`;
+    return new Response(JSON.stringify(response), { status: 200 });
+}
 
 //Modify task - Chris
+
 
 //Delete all tasks - Nathan
