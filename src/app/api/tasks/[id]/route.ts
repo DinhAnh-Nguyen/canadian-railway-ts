c6aa8f5e62e611ad6697f4c66bd7374cda0b185b
DinhAnh2301 <dinhanh2301@gmail.com>
2025-01-17 14:49:20 -0700

    add tasks function added

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,6 +1,21 @@
+import { neon } from "@neondatabase/serverless";
 
-//Add a Schedule - Daniel
+// Add a Schedule - Daniel
+export async function GET({ params }: { params: { id: Number | String } }) {
+    const databaseUrl = process.env.DATABASE_URL || ""; // Set a default value if DATABASE_URL is not defined
+    const sql = neon(databaseUrl);
+    //PostgresQL
+    const id = Number(params.id);
+    const response = await sql`SELECT * FROM tasks WHERE id = ${id};`;
+    const user = response[0];
 
-//Modify a Schedule - Chris
+    if (!user) {
+        return new Response(null, { status: 404 });
+    }
 
-//Delete a Schedule - Nathan
+    return new Response(JSON.stringify(response), { status: 200 });
+}
+
+// Modify a Schedule - Chris
+
+// Delete a Schedule - Nathan
--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,7 +1,17 @@
-//Fetch All Schedules - Mark
+import { neon } from "@neondatabase/serverless";
+
+// Fetch All Schedules - Mark
 
 // Add task - Daniel
+export async function POST(request: Request) {
+    const resquestData = await request.json();
+    const databaseUrl = process.env.DATABASE_URL || ""; // Set a default value if DATABASE_URL is not defined
+    const sql = neon(databaseUrl);
+    //PostgresQL
+    const response = await sql`INSERT INTO tasks (description, status, Assigned_to, created_by, due_date, priority) VALUES (${resquestData.name}, ${resquestData.email}, ${resquestData.role});`;
+    return new Response(JSON.stringify(response), { status: 200 });
+}
 
-//Modify task - Chris
+// Modify task - Chris
 
-//Delete all tasks - Nathan
+// Delete all tasks - Nathan
