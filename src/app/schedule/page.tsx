cb07c0b3d3d11bba1583f8cbb88b350da1a65556
Chris Williams <cchriswilliams721@gmail.com>
2025-01-17 14:02:51 -0700

    task map

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -72,6 +72,7 @@ export default function Schedule() {
             /* handle save */
           }}
         />
+      
       </div>
     </div>
   );
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -132,7 +132,8 @@ export default function Modal({isOpen, onClose}: ModalProps) {
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               >
                 {users.map((user) => (
-                  <option key={user.id} value={user.id}>
+                  //Based on role
+                  <option key={`${user.id}-${user.role}`} value={user.id}>
                     {user.name}
                   </option>
                 ))}
