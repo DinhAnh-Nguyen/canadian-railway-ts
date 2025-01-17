dece38b8d846ded656d1ae1626cc39402e2f5afb
bnhao <88442790+bnhao@users.noreply.github.com>
2025-01-17 15:06:26 -0700

    fix variable names

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -3,13 +3,13 @@ import { neon } from "@neondatabase/serverless";
 //Add a task - Daniel
 
 //Modify a task - Chris
-export async function PUT( request : Request, { params }: { params: { id: Number | String} }) {
+export async function PUT(request: Request, { params }: { params: { id: Number | String } }) {
     const databaseUrl = process.env.DATABASE_URL || ""; // Set a default value if DATABASE_URL is not defined
     const sql = neon(databaseUrl);
     //PostgresQL
     const id = Number(params.id);
     const resquestData = await request.json();
-    const response = await sql`UPDATE schedules SET description = ${resquestData.description}, status = ${resquestData.status}, due_date = ${resquestData.due_date}, piroty = ${resquestData.piroty} WHERE id = ${id};`;;
+    const response = await sql`UPDATE schedules SET description = ${resquestData.description}, status = ${resquestData.status}, due_date = ${resquestData.due_date}, piroty = ${resquestData.piroty} WHERE id = ${id};`;
     return new Response(JSON.stringify(response), { status: 200 });
 }
 
@@ -24,5 +24,5 @@ export async function DELETE(request: Request, { params }: { params: { id: numbe
         return new Response(null, { status: 404 }); //When Task Not Found
     }
 
-    return new Response(null, { status: 200 }); 
+    return new Response(null, { status: 200 });
 }
--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -16,11 +16,19 @@ export async function POST(request: Request) {
     const databaseUrl = process.env.DATABASE_URL || ""; // Set a default value if DATABASE_URL is not defined
     const sql = neon(databaseUrl);
     //PostgresQL
-    const response = await sql`INSERT INTO schedules (description, status, due_date, piroty) VALUES (${resquestData.description}, ${resquestData.status}, ${resquestData.due_date}, ${resquestData.piroty});`;
+    const response = await sql`INSERT INTO schedules (description, status, due_date, priority) VALUES (${resquestData.description}, ${resquestData.status}, ${resquestData.due_date}, ${resquestData.piroty});`;
     return new Response(JSON.stringify(response), { status: 200 });
 }
 
 //Modify task - Mark 
+export async function PUT(request: Request) {
+    const databaseUrl = process.env.DATABASE_URL || ""; // Set a default value if DATABASE_URL is not defined
+    const sql = neon(databaseUrl);
+    //PostgresQL
+    const resquestData = await request.json();
+    const response = await sql`UPDATE schedules SET description = ${resquestData.description}, status = ${resquestData.status}, due_date = ${resquestData.due_date}, priotity = ${resquestData.priotity} WHERE id = ${resquestData.id};`;
+    return new Response(JSON.stringify(response), { status: 200 });
+}
 
 
 //Delete all tasks - Nathan Delete all tasks from table. Keep table.
@@ -31,5 +39,5 @@ export async function DELETE() {
 
     const response = await sql`DELETE FROM schedules;`;
 
-    return new Response(null, { status: 200 }); 
+    return new Response(null, { status: 200 });
 }
