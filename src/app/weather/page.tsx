d54ca6104c9aa2e778a5255bc894914ce8d55316
bnhao <88442790+bnhao@users.noreply.github.com>
2025-01-19 21:20:00 -0700

    FE Mock Up

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -26,7 +26,7 @@ export default function Weather() {
           {/* Dropdown Menu */}
           <div className='flex justify-between items-center bg-emerald-950 p-4 rounded'>
             <h2 className='text-lg font-semibold'>Select a Track</h2>
-            <select className='py-4 px-2 bg-gray-800 rounded'>
+            <select className='py-4 px-2 bg-gray-800 rounded fill-current'>
               <option value='1'>Track 1</option>
               <option value='2'>Track 2</option>
               <option value='3'>Track 3</option>
@@ -34,6 +34,31 @@ export default function Weather() {
               <option value='5'>Track 5</option>
               <option value='6'>Track 6</option>
             </select>
+          </div>
+
+          <div className='grid grid-cols-12 grid-rows-3 gap-6'>
+            {/* Wind DirectionGraph */}
+            <div className='col-span-6 row-span-1 p-4 rounded'>
+              <h2 className='text-lg font-semibold bg-emerald-950 px-4 py-2 rounded-2xl'>Wind Direction for Selected Track</h2>
+              <div className='h-40 bg-gray-800 rounded mt-4'></div>
+            </div>
+
+            {/* Wind Speed Graph */}
+            <div className='col-span-6 row-span-2 p-4 rounded grid-flow-col-dense'>
+              <h2 className='text-lg font-semibold bg-emerald-950 px-4 py-2 rounded-2xl'>Map view for Selected Track</h2>
+              <div className='h-5/6 row-span-2 bg-gray-800 rounded mt-4'></div>
+            </div>
+
+            <div className='col-span-6 row-span-1 0 p-4 rounded'>
+              <h2 className='text-lg font-semibold bg-emerald-950 px-4 py-2 rounded-2xl'>Wind Direction for Selected Track</h2>
+              <div className='h-40 bg-gray-800 rounded mt-4'></div>
+            </div>
+
+            <div className='col-span-6 w-full col-span-12  p-4 rounded'>
+              <h2 className='text-lg font-semibold bg-emerald-950 px-4 py-2 rounded-2xl'>Wind Direction for Selected Track</h2>
+              <div className='h-40 w-full col-span-12 bg-gray-800 rounded mt-4'></div>
+            </div>
+
           </div>
         </div>
       </div>
