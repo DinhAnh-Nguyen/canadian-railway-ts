fc9534c465dc5d0b8ffb97dc4931c7a57dcfbac6
bnhao <88442790+bnhao@users.noreply.github.com>
2025-01-23 13:08:55 -0700

    update

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -9,6 +9,7 @@ import useForecast from '../hooks/useForecast';
 const App = (): JSX.Element => {
   const { selectedTrack, handleTrackChange, forecastData } = useForecast();
 
+  return (
     <div className="bg-black text-white min-h-screen p-6">
       <SelectLocation
         track={selectedTrack}
